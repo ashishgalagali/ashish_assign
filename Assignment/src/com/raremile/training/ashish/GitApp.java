2623ecada89558abcb106cb255df1aba5a173938
Sriram Keerthi M K <sriram@raremile.com>
2015-06-24 16:53:24 +0530

    Update GitApp.java

--- Assignment/src/com/raremile/training/ashish/GitApp.java
+++ Assignment/src/com/raremile/training/ashish/GitApp.java
@@ -4,6 +4,6 @@ public class GitApp
 {
 	public static void main(String[] args)
 		{ 
-	      System.out.println("My name is Ashish.I'm learning Git");
+	      System.out.println("My name is Ashish.I'm learning Git. File modified for part 3.");
 	   }
 }
